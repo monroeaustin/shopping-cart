b832d8ca00f9aa7e5be88181b2c90a20b1d413b4
Monroe Austin <monroeraustin2@yahoo.com>
2025-07-02 15:38:28 -0700

    style: Header/Navbar Color Change to Orange on Hov

--- shopping-cart/src/components/Header/Header.jsx
+++ shopping-cart/src/components/Header/Header.jsx
@@ -15,25 +15,29 @@ function Header() {
     <>
       <header className="fixed top-0 left-0 w-full px-18 py-5 z-[100] bg-black/15 backdrop-blur-md text-white flex items-center justify-between shadow-md">
         <div className="text-2xl font-bold cursor-pointer">
-          <Link to="/" className="hover:text-blue-300 transition flex items-center gap-1 cursor-pointer">
-                Panto
-              </Link>
+          <Link to="/" className="hover:text-orange-500
+ transition flex items-center gap-1 cursor-pointer">
+            Panto
+          </Link>
         </div>
 
         <nav>
           <ul className="flex gap-10">
             <li>
-              <Link to="/" className="hover:text-blue-300 transition flex items-center gap-1 cursor-pointer">
+              <Link to="/" className="hover:text-orange-500
+ transition flex items-center gap-1 cursor-pointer">
                 Furniture <RiArrowDropDownLine className="text-2xl" />
               </Link>
             </li>
             <li>
-              <Link to="/shop" className="hover:text-blue-300 transition cursor-pointer">
+              <Link to="/shop" className="hover:text-orange-500
+ transition cursor-pointer">
                 Shop
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-blue-300 transition cursor-pointer">
+              <Link to="/" className="hover:text-orange-500
+ transition cursor-pointer">
                 About Us
               </Link>
             </li>
